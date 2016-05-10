36da4018409978d32ddf201a022a6e21fd1c813c
Benjy Cui <benjytrys@gmail.com>
2016-05-10 16:21:23 +0800

    Updates

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ webpackJsonp([1,0],[
 	var routes = theme.completedRoutes.map(function (item, index) {
 	  return React.createElement(Route, {
 	    key: index,
-	    path: item.route,
+	    path: '/' + item.route,
 	    component: templateWrapper(item.template, item.dataPath)
 	  });
 	});
