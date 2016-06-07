600663170e584010e15cd63138d3e5a1710618cf
Benjy Cui <benjytrys@gmail.com>
2016-06-07 09:19:47 +0800

    Updates

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-webpackJsonp([1,2],{245:function(e,t){"use strict";e.exports={content:["article",{},["p","Good bye!"]],meta:{title:"Good bye!",publishDate:"2015-05-05T00:00:00.000Z",filename:"posts/good-bye.md"},description:["section",["p","The second article which is posted by BiSheng."]]}}});
+webpackJsonp([1,2],{243:function(e,o){e.exports={content:["article",{},["p","Good bye!"]],meta:{title:"Good bye!",publishDate:"2015-05-05T00:00:00.000Z",filename:"posts/good-bye.md"},description:["section",["p","The second article which is posted by BiSheng."]]}}});
